7737b3012aa6f7e384a1ec08287694c776e50fed
Roy <rjperlman@gmail.com>
2024-11-20 12:05:47 -0800

    Make Some Updates

--- index.js
+++ index.js
@@ -8,3 +8,34 @@ const freelancers = [
     { name: "Dr. Wire", price: 47, occupation: "teacher" },
     { name: "Prof. Goose", price: 72, occupation: "driver" },
   ];
+
+  const name = ["Dr. Slice", "Dr. Pressure", "Prof. Possibility", "Prof. Prism","Dr. Impulse", "Prof. Spark", "Dr. Wire", "Prof. Goose"];
+  const occupation = ["gardener", "programmer", "teacher", "driver"];
+
+
+  function render()
+  {
+
+
+        averagePrice();
+  }
+
+  function addfreelancers()
+  {
+    const listOfFreelancers = document.querySelector('freelancerList');
+    listOfFreelancers.innerHTML = '';
+
+  }
+
+
+  function averagePrice()
+  {
+    const theAveragePriceOfFreelancers = 
+        freelancers.reduce((accumlator, currentValue) =>
+            accumlator + currentValue.price, 0);
+    const totalAveragePrice = (theAveragePriceOfFreelancers/freelancers.length);
+    return totalAveragePrice.toFixed(2);
+  }
+
+  render();
+
