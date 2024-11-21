0c172509554581bbcde4205e01b08932d614a985
Roy <rjperlman@gmail.com>
2024-11-20 22:28:08 -0800

    Fixed lots of bugs and the loop works very well.

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
+/**
+ * The group of freelancers serves as an example for the workshop.
+ */
 const freelancers = [
-    { name: "Dr. Slice", price: 25, occupation: "gardener" },
-    { name: "Dr. Pressure", price: 51, occupation: "programmer" },
-    { name: "Prof. Possibility", price: 43, occupation: "teacher" },
-    { name: "Prof. Prism", price: 81, occupation: "teacher" },
-    { name: "Dr. Impulse", price: 43, occupation: "teacher" },
-    { name: "Prof. Spark", price: 76, occupation: "programmer" },
-    { name: "Dr. Wire", price: 47, occupation: "teacher" },
-    { name: "Prof. Goose", price: 72, occupation: "driver" },
+    { name: "Dr. Slice", price: 25, occupation: "Gardener" },
+    { name: "Dr. Pressure", price: 51, occupation: "Programmer" },
+    { name: "Prof. Possibility", price: 43, occupation: "Teacher" },
+    { name: "Prof. Prism", price: 81, occupation: "Teacher" },
+    { name: "Dr. Impulse", price: 43, occupation: "Teacher" },
+    { name: "Prof. Spark", price: 76, occupation: "Programmer" },
+    { name: "Dr. Wire", price: 47, occupation: "Teacher" },
+    { name: "Prof. Goose", price: 72, occupation: "Driver" },
   ];
 
+  // The array of names of each freelancer.
   const Name = ["Dr. Slice", "Dr. Pressure", "Prof. Possibility", "Prof. Prism","Dr. Impulse", "Prof. Spark", "Dr. Wire", "Prof. Goose"];
-  const Occupation = ["gardener", "programmer", "teacher", "driver"];
+  // An array of job titles, which are called occupation.
+  const Occupation = ["Gardener", "Programmer", "Teacher", "Driver"];
+  //Set the max of freelances to 50.
+  const theMax = 50;
 
+  /**
+   * This function generates a random order for freelancers. 
+   * @returns {const} random numbers for each name, occupation, and price.
+   */
+  function randomGenerator()
+  {
+    const randomName = Name[Math.floor(Math.random() * Name.length)];
+    const randomOccupation = Occupation[Math.floor(Math.random() * Occupation.length)];
+    const randomPrice = Math.floor(Math.random() * 90) + 20;
+
+    return {name: randomName, occupation: randomOccupation, price: randomPrice };
+  }
+
+  /**
+   * This function will render the existing freelancers from the given array while
+   * generating some new random ones. It will also calculate the average price of 
+   * all the freelancers provided.
+   */
   function render()
   {
     const listOfFreelancers = document.querySelector('#freelancerList');
@@ -20,41 +45,31 @@ const freelancers = [
     for(let i = 0; i < freelancers.length; i++)
     {
         const freelancer = freelancers[i];
-        const freelancerElement = document.createElement('div');
-        freelancerElement.classList.add('freelancerSpaceing');
-        freelancerElement.innerHTML = `<strong>${freelancer.name}</strong> <strong>${freelancer.occupation}</strong>  $${freelancer.price}`;
+        const freelancerElement = document.createElement('tr');
+        freelancerElement.innerHTML = `<td>${freelancer.name}</td><td>${freelancer.occupation}</td><td>$${freelancer.price}</td></tr>`;
         listOfFreelancers.appendChild(freelancerElement);
     }
-    averagePrice();
-  }
 
-  function radomGenerator()
-  {
-    const randomName = Name[Math.floor(Math.random() * Name.length)];
-    const randomOccupation = Occupation[Math.floor(Math.random() * Occupation.length)];
-    const randomPrice = Math.floor(Math.random() * 100) + 20;
-
-    return {name: randomName, occupation: randomOccupation, price: randomPrice };
-  }
-
-
-  function addfreelancers()
-  {
-    const nextFreelancer = radomGenerator();
-    freelancers.push(nextFreelancer);
-    render();
-  }
-
-  function averagePrice()
-  {
     const theAveragePriceOfFreelancers = 
         freelancers.reduce((accumlator, currentValue) =>
             accumlator + currentValue.price, 0);
-    const totalAveragePrice = (theAveragePriceOfFreelancers/freelancers.length);
-    document.querySelector('#averagePrice').innerText = `Average Starting Price: $${totalAveragePrice.toFixed(2)}`;
+    const totalAveragePrice = ((theAveragePriceOfFreelancers/freelancers.length).toFixed(2));
 
+    document.querySelector('#averagePrice').innerText = `The Average Starting Price is $${totalAveragePrice}`;
   }
 
-  render();
-
-  setInterval(addfreelancers, 3000);
+  /**
+   * Adding new freelancers will occur at intervals, allowing the list of freelancers to render. 
+   * The loop will stop when the number of freelancers is greater than or equal to the given maximum.
+   */
+  const addfreelancers = setInterval (()=>
+  {
+    const nextFreelancer = randomGenerator();
+    freelancers.push(nextFreelancer);
+    render();
+    if(freelancers.length >= theMax)
+    {
+        clearInterval(addfreelancers);
+        return;
+    }
+  }, 40);
